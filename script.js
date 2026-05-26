2512fb2dafbb8e41b54db0eff05310954a162f84
vozvisual <vitorfabiano1008@gmail.com>
2026-05-25 20:12:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-// Script do Voz Visual
-console.log("Site Voz Visual carregado com sucesso!");
+function handleGoogleLogin(response){
+  const payload = JSON.parse(atob(response.credential.split('.')[1]));
 
-// Futuras funções podem ser adicionadas aqui
+  const user = {
+    name: payload.name,
+    email: payload.email,
+    avatar: payload.picture,
+    admin: false
+  };
+
+  localStorage.setItem('vv_user', JSON.stringify(user));
+
+  toast('Login Google realizado com sucesso!','ok');
+
+  loadUser();
+  go('galeria');
+}
